c748d227d052541d6946f2c39ce99a9f1bffbbd3
RFDaemoniac <robinson.farrar@gmail.com>
2013-10-24 11:25:46 -0400

    The project.
    Get to the chopper.

---
+++ 3 minute dodge/Assets/Cube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -10) {
+			Destroy(gameObject);
+		}
+	}
+}
