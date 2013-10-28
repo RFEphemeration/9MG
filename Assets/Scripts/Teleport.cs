aed63c4d2edbc080d5242fcf5ea9b7cd96546be9
Matthew Adler <madler72@gmail.com>
2013-10-27 23:11:46 -0400

    Starting to implement teleport.

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+	
+	float startTime;
+	float range; 
+	Vector3 direction;
+	bool startedCounting;
+	
+	void Start()
+	{
+		startedCounting = false;
+		range = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		if ((/*Input.GetAxis("Joystick 10th") > 0  ||*/ Input.GetKeyDown(KeyCode.Space)) && !startedCounting)
+		{
+			startedCounting = true;
+			startTime = Time.time;	
+		}
+		if (/*Input.GetAxis("Joystick 10th") <= 0 ||*/ Input.GetKeyUp(KeyCode.Space))
+		{
+			range = Time.time - startTime;
+			range = Mathf.Min(range * 3.0f, 5.0f);
+			direction = (new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y")));
+			direction = (direction - transform.position).normalized * range;
+			teleportDirection(direction);
+			startedCounting = false;
+		}
+		
+	}
+	
+	public void teleportDirection(Vector3 d)
+	{
+		transform.position += d;	
+	}
+}
