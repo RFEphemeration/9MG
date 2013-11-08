7660921862bc279f6a23725d6c19ff47c4d6d057
Matthew Adler <madler72@gmail.com>
2013-11-08 14:39:17 -0500

    Added new enemy: Log. Log spawns on edges of the level and sweeps across to the other side. It doesn’t have a leave state so when boomed it is barely affected.
    Added two new test levels.
    
    Increased gravity on player sphere
    
    Modified move’s boom method to check if the “child” is a cube before setting its state.

---
+++ Assets/Scripts/LogsSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogsSpawner : MonoBehaviour {
+	
+	
+	public float delay = 10.0f;
+	public GameObject log;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn", delay, delay);
+	}
+	
+	void Spawn () {
+		
+		Vector3 position;
+		
+		float randomPos = Random.Range (0, 100);
+		
+		if (randomPos >= 75)
+		{
+			position = new Vector3(10, 3, Random.Range (-7,7));
+			Instantiate(log, position, log.transform.rotation);
+		}
+		else if (randomPos >= 50)
+		{
+			position = new Vector3(-10, 3, Random.Range(-7, 7));
+			Instantiate(log, position, log.transform.rotation);
+		}
+		else if (randomPos >= 25)
+		{
+			position = new Vector3(Random.Range(-7, 7), 3, 10);
+			Instantiate(log, position, Quaternion.Euler(0.0f, 0.0f, 90.0f));
+		}
+		else
+		{
+			position = new Vector3(Random.Range(-7, 7), 3, -10);
+			Instantiate(log, position, Quaternion.Euler(0.0f, 0.0f, 90.0f));
+		}
+		
+	}
+	
+}
